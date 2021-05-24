64b24192d549861787f2b7b90b8ebd33eedfa6ac
Brigh Mensah <bright.kofi.mensah@gmail.com>
2021-05-24 22:59:33 +0200

    CRUD for Question

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'questions#index'
 
   # index, show, new, create, update, destroy,
+  # get "/questions", to: "questions#index"
   resources :questions, except: [:destroy, :update]
 end
